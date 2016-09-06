ef68ab0eabdc844e4bd30ae05889cf521fde3e74
Daniel <0xc0decafe@users.noreply.github.com>
2016-09-06 14:45:31 +0200

    Added shell script to automate build process.

---
+++ prepare_n_build.sh
@@ -0,0 +1,45 @@
+#!/bin/sh -x
+
+git clone https://github.com/ernw/ss7MAPer
+
+cd ss7MAPer
+rebar get-deps
+
+cd deps/osmo_map   
+patch -p1 < ../../patches/osmo_map.patch   
+cd ../osmo_sccp   
+patch -p1 < ../../patches/osmo_sccp.patch   
+cd ../osmo_ss7   
+patch -p1 < ../../patches/osmo_ss7.patch 
+
+cd ../../
+sed -i 's/DRV_CFLAGS/CFLAGS/g' deps/epcap/rebar.config
+sed -i 's/DRV_LDFLAGS/LDFLAGS/g' deps/epcap/rebar.config
+
+cd deps
+git clone http://cgit.osmocom.org/erlang/signerl/
+
+cd signerl/TCAP/asn_src/ITU
+make
+
+cd ../../../../..
+cp deps/signerl/TCAP/asn_src/ITU/*rl deps/osmo_sccp/src/
+
+mkdir deps/epcap/deps
+ln -sd ../../pkt deps/epcap/deps/pkt
+mkdir deps/osmo_sccp/deps   
+ln -sd ../../osmo_ss7 deps/osmo_sccp/deps/osmo_ss7   
+ln -sd ../../epcap deps/osmo_sccp/deps/epcap   
+ln -sd ../../pkt deps/osmo_sccp/deps/pkt   
+ln -sd ../../signerl/MAP deps/osmo_sccp/deps/MAP    
+ln -sd ../../signerl/SCCP deps/osmo_sccp/deps/SCCP   
+ln -sd ../../signerl/TCAP deps/osmo_sccp/deps/TCAP   
+mkdir deps/osmo_map/deps   
+ln -sd ../../osmo_ss7 deps/osmo_map/deps/osmo_ss7   
+ln -sd ../../epcap deps/osmo_map/deps/epcap   
+ln -sd ../../pkt deps/osmo_map/deps/pkt
+
+cp deps/signerl/SCCP/itu/include/sccp.hrl deps/osmo_sccp/src/   
+cp deps/signerl/TCAP/include/tcap.hrl deps/osmo_map/src/
+
+rebar co
